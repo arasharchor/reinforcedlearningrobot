8cd4ea30a18330eebae6ac8c190bd11f3e9cccd9
Martin Sand <martin.sand.nielsen@gmail.com>
2016-10-03 15:36:36 +0200

    Simple camera_node.py for test

--- catkin_ws/src/dlrobot/src/camera_node.py
+++ catkin_ws/src/dlrobot/src/camera_node.py
@@ -7,11 +7,10 @@ import sys
 caffe_root = '~/caffe'
 sys.path.insert(0, caffe_root + 'python')
 
+
 class camera_node:
-    
     def __init__(self):
         rospy.init_node('camera_node')
-
         self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
         self.bridge = CvBridge()
         self.logfile = open('controls.log', 'w')
@@ -23,23 +22,19 @@ class camera_node:
     def __del__(self):
         self.logfile.close()
 
-
     def callback(self, data):
-        print "callback image received"
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            filename = 'img_database/colorImg_%s.png' % self.imgnr
-            cv2.imwrite(filename, cv_image)
-            self.imgnr += 1
-            buf = "%f; %f; colorImg_%s\n" % (x, z,self.imgnr)
-            self.logfile.write(buf)
-            print filename
+        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        filename = 'img_database/colorImg_%s.png' % self.imgnr
+        cv2.imwrite(filename, cv_image)
+        self.imgnr += 1
+        buf = "%f; %f; colorImg_%s\n" % (0, 0, self.imgnr)
+        self.logfile.write(buf)
+        print filename
 
 
 def main(args):
-
     ic = camera_node()
     try:
-
         rospy.spin()
     except KeyboardInterrupt:
         print "Shutting down"
